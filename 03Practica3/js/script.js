d157bb373f046d7757787ce386229b0778892808
vixu92 <vixu92@yahoo.es>
2018-05-22 12:04:12 +0200

    Uno mas

---
+++ 03Practica3/js/script.js
@@ -0,0 +1,85 @@
+function comienzo(){
+
+    let p1 = document.getElementById('p1');
+
+    setTimeout(function ir(){
+        p1.style.backgroundColor = "rgb(0,0,255,0.2)";},500);
+
+    setTimeout(function volver(){
+        p1.style.backgroundColor = "transparent";},1000);
+
+    setTimeout(function ir2(){
+        p1.style.backgroundColor = "rgb(0,0,255,0.2)";},1500);
+    
+    setTimeout(function volver2(){
+        p1.style.backgroundColor = "transparent";},2000);
+        
+    setTimeout(function ir2(){
+        p1.style.backgroundColor = "rgb(0,0,255,0.2)";},2500);
+    
+    setTimeout(function volver2(){
+        p1.style.backgroundColor = "transparent";},3000);        
+}
+
+function jazz(){
+
+    setTimeout(function ir(){
+        document.getElementById('p2').style.backgroundColor = "rgb(255,0,0,0.2)";},500);
+
+    setTimeout(function volver(){
+        document.getElementById('p2').style.backgroundColor = "transparent";},1000);
+
+    setTimeout(function ir2(){
+        document.getElementById('p2').style.backgroundColor = "rgb(255,0,0,0.2)";},1500);
+    
+    setTimeout(function volver2(){
+        document.getElementById('p2').style.backgroundColor = "transparent";},2000);  
+
+    setTimeout(function ir3(){
+        document.getElementById('p2').style.backgroundColor = "rgb(255,0,0,0.2)";},2500);
+        
+    setTimeout(function volver3(){
+        document.getElementById('p2').style.backgroundColor = "transparent";},3000);               
+}
+
+function circulo(){
+    setTimeout(function ir(){
+        document.getElementById('p3').style.backgroundColor = "rgb(0,255,0,0.2)";},500);
+
+    setTimeout(function volver(){
+        document.getElementById('p3').style.backgroundColor = "transparent";},1000);
+
+    setTimeout(function ir2(){
+        document.getElementById('p3').style.backgroundColor = "rgb(0,255,0,0.2)";},1500);
+    
+    setTimeout(function volver2(){
+        document.getElementById('p3').style.backgroundColor = "transparent";},2000); 
+
+    setTimeout(function ir3(){
+        document.getElementById('p3').style.backgroundColor = "rgb(0,255,0,0.2)";},2500);
+        
+    setTimeout(function volver3(){
+        document.getElementById('p3').style.backgroundColor = "transparent";},3000);     
+}
+
+function intro(){
+    setTimeout(function ir(){
+        document.getElementById('intro').style.backgroundColor = "rgb(255,255,0,0.2)";},500);
+
+    setTimeout(function volver(){
+        document.getElementById('intro').style.backgroundColor = "transparent";},1000);
+
+    setTimeout(function ir2(){
+        document.getElementById('intro').style.backgroundColor = "rgb(255,255,0,0.2)";},1500);
+    
+    setTimeout(function volver2(){
+        document.getElementById('intro').style.backgroundColor = "transparent";},2000);  
+
+    setTimeout(function ir3(){
+        document.getElementById('intro').style.backgroundColor = "rgb(255,255,0,0.2)";},2500);
+        
+    setTimeout(function volver3(){
+        document.getElementById('intro').style.backgroundColor = "transparent";},3000);         
+}
+
+    
