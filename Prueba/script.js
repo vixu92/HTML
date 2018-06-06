c0f30aa84ba0a8fa7ecfab7901427a039be3ae96
vixu92 <vixu92@yahoo.es>
2018-06-06 11:19:49 +0200

    Fin

---
+++ Prueba/script.js
@@ -0,0 +1,13 @@
+function siPieFoto(){
+
+    var pie = document.getElementById("pie");
+
+   pie.style.visibility = "visible"
+
+}
+function noPieFoto(){
+    var pie = document.getElementById("pie");
+
+   pie.style.visibility = "hidden"
+
+}
